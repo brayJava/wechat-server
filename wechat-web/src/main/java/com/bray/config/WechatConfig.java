04d448bd36a56973986432ac6d683ec00f0f59b6
bray <goodboy_bray@163.com>
2018-09-18 00:43:32 +0800

    修改

--- wechat-web/src/main/java/com/bray/config/WechatConfig.java
+++ wechat-web/src/main/java/com/bray/config/WechatConfig.java
@@ -1,13 +1,21 @@
 package com.bray.config;
 
+import com.bray.dto.EffectiveType;
+import com.bray.mapper.WyWechatAuthMapper;
+import com.bray.model.WyWechatAuth;
+import com.bray.model.WyWechatAuthExample;
 import com.foxinmy.weixin4j.cache.FileCacheStorager;
 import com.foxinmy.weixin4j.cache.MemcacheCacheStorager;
 import com.foxinmy.weixin4j.model.Token;
 import com.foxinmy.weixin4j.model.WeixinAccount;
 import com.foxinmy.weixin4j.mp.WeixinProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Author:wuzhiyuan
@@ -24,17 +32,24 @@ public class WechatConfig {
     private String weixinId;
     @Value("${weixin4j.secret}")
     private String weixinSecret;
+    @Resource
+    private WyWechatAuthMapper wyWechatAuthMapper;
     /**
      * 实例化微信代理类
      * @return
      */
     @Bean
     public WeixinProxy initWeixinProxy() {
+        WyWechatAuthExample wyWechatAuthExample = new WyWechatAuthExample();
+        wyWechatAuthExample.createCriteria().andStatusEqualTo(EffectiveType.EFFECTIVE_YES);
+        wyWechatAuthExample.setOrderByClause("update_time desc limit 1");
+        List<WyWechatAuth> wyWechatAuths = wyWechatAuthMapper.selectByExample(wyWechatAuthExample);
+        WyWechatAuth wyWechatAuth = wyWechatAuths.get(0);
         WeixinProxy weixinProxy = null;
-        if("FILE".equals(weixinCache)) {
-            weixinProxy = new WeixinProxy(new WeixinAccount(weixinId,weixinSecret),new FileCacheStorager<Token>());
+        if("FILE".equals(wyWechatAuth.getWeixinCache())) {
+            weixinProxy = new WeixinProxy(new WeixinAccount(wyWechatAuth.getWeixinId(),wyWechatAuth.getWeixinSecret()),new FileCacheStorager<Token>());
         } else {
-            weixinProxy = new WeixinProxy(new WeixinAccount(weixinId,weixinSecret),new MemcacheCacheStorager<Token>());
+            weixinProxy = new WeixinProxy(new WeixinAccount(wyWechatAuth.getWeixinId(),wyWechatAuth.getWeixinSecret()),new MemcacheCacheStorager<Token>());
         }
         return weixinProxy;
     }
