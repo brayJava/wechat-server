cd8174827182b0de1ff52a79d8b748bdee7d968d
bray <goodboy_bray@163.com>
2018-09-19 02:01:18 +0800

    修改

--- wechat-web/src/main/java/com/bray/web/controller/WechatOrderController.java
+++ wechat-web/src/main/java/com/bray/web/controller/WechatOrderController.java
@@ -6,11 +6,15 @@ import com.bray.service.IOrderWebService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 /**
@@ -61,7 +65,7 @@ public class WechatOrderController {
 
     private void sendEmailWithOrder(OrderModelVo orderModelVo) {
         StringBuilder orderBuf = new StringBuilder();
-        orderBuf.append("------------新的订单------------- \n");
+//        orderBuf.append("------------新的订单------------- \n");
         orderBuf.append("姓名："+orderModelVo.getName()+"\n");
         orderBuf.append("电话："+orderModelVo.getPhone()+"\n");
         orderBuf.append("城市："+orderModelVo.getProvince()+orderModelVo
@@ -69,17 +73,30 @@ public class WechatOrderController {
         orderBuf.append("地址："+orderModelVo.getAddress()+"\n");
         orderBuf.append("商品类型："+orderModelVo.getTitle()+"\n");
         orderBuf.append("尺寸大小："+orderModelVo.getSize());
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setSubject("有新的订单");
-        simpleMailMessage.setText(orderBuf.toString());
-        simpleMailMessage.setTo("1318134732@qq.com");
-        simpleMailMessage.setFrom("goodboy_bray@163.com");
-        Properties prop = new Properties();
-        //解决ip与域名不匹配的问题
-        prop.put("mail.smtp.localhost","mail.163.com");
-        prop.put("mail.smtp.auth", "false");
-//        prop.put("mail.smtp.port", "25");
-        javaMailSender.setJavaMailProperties(prop);
-        javaMailSender.send(simpleMailMessage);
+        MimeMessage msg = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        try {
+            helper = new MimeMessageHelper(msg, true,"utf-8");
+            helper.setFrom("goodboy_bray@163.com");
+            helper.setCc("goodboy_bray@163.com");
+            helper.setTo("1318134732@qq.com");
+            helper.setText(orderBuf.toString());
+            helper.setSubject("来了新的订单");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//        simpleMailMessage.setSubject("有新的订单");
+//        simpleMailMessage.setText(orderBuf.toString());
+//        simpleMailMessage.setTo("1318134732@qq.com");
+//        simpleMailMessage.setFrom("goodboy_bray@163.com");
+//        Properties prop = new Properties();
+//        //解决ip与域名不匹配的问题
+//        prop.put("mail.smtp.localhost","mail.163.com");
+//        prop.put("mail.smtp.auth", "false");
+////        prop.put("mail.smtp.port", "25");
+//        javaMailSender.setJavaMailProperties(prop);
+        javaMailSender.send(msg);
     }
 }
