6812b005c6a06181b3b5203a93a6f03e004e7112
bray <goodboy_bray@163.com>
2018-09-20 23:33:28 +0800

    修改

--- wechat-web/src/main/java/com/bray/web/controller/WechatController.java
+++ wechat-web/src/main/java/com/bray/web/controller/WechatController.java
@@ -5,22 +5,28 @@ import com.bray.config.WebConst;
 import com.bray.dto.ConstFinal;
 import com.bray.dto.UrlConstant;
 import com.bray.model.Bo.ArticleWithImages;
+import com.bray.model.WyOrder;
 import com.bray.service.IArticleService;
 import com.bray.service.IDomainWebService;
 import com.bray.service.impl.WechatAuthServiceImpl;
 import com.bray.util.Base64Util;
+import com.bray.util.DateUtil;
 import com.bray.util.WechatUtil;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.time.Clock;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -44,7 +50,7 @@ public class WechatController {
      * 首页跳转
      */
     @RequestMapping("/jump/{articleId}/{temstamp}")
-    String index(@PathVariable String articleId,@PathVariable String temstamp, Model model) {
+    public String index(@PathVariable String articleId,@PathVariable String temstamp, Model model) {
         Map<String,Object> domainMap = (HashMap<String,Object>)iDomainWebService.queryDomainByredisServer(ConstFinal.DOMAIN_MAP);
         //获取内容跳转链接
         JSONObject jsonObject = WechatUtil.nextUrlBuild(WebConst.SUB_COMMON_DOMAIN, UrlConstant.PATH_CONTENT_URL,articleId,domainMap);
@@ -59,7 +65,7 @@ public class WechatController {
      * 内容跳转
      */
     @RequestMapping("/content/{articleId}/{timstamp}")
-    String content(@PathVariable String articleId, String timstamp, Model model, HttpServletRequest request) {
+    public String content(@PathVariable String articleId, String timstamp, Model model, HttpServletRequest request) {
         Map<String,Object> domainMap = (HashMap<String,Object>)iDomainWebService.queryDomainByredisServer(ConstFinal.DOMAIN_MAP);
         JSONObject jsonObject = WechatUtil.nextUrlBuild(WebConst.SUB_SHARE_DOMAIN, UrlConstant.PATH_JUMP_RUL,articleId, domainMap);
         String shareUrl = String.valueOf(jsonObject.get("url"));
@@ -76,7 +82,7 @@ public class WechatController {
      * 内容跳转
      */
     @RequestMapping("/random-content/{articleId}/{timstamp}")
-    String randomContent(@PathVariable String articleId,@PathVariable String timstamp,Model model, HttpServletRequest request) {
+    public ModelAndView randomContent(@PathVariable String articleId,@PathVariable String timstamp,Model model, HttpServletRequest request) {
         Map<String,Object> domainMap = (HashMap<String,Object>)iDomainWebService.queryDomainByredisServer(ConstFinal.DOMAIN_MAP);
         JSONObject jsonObject = WechatUtil.nextUrlBuild(WebConst.SUB_SHARE_DOMAIN, UrlConstant.PATH_JUMP_RUL,articleId, domainMap);
         String shareUrl = String.valueOf(jsonObject.get("url"));
@@ -87,13 +93,14 @@ public class WechatController {
         model.addAttribute("article",articleWithImages);
 //        model.addAttribute("signature",signature);
         log.info("----------分享链接为：{}",shareUrl);
-        return "html/random-content";
+        ModelAndView modelAndView =  new ModelAndView("redirect:http://localhost:8081/html/random-common.html");
+        return modelAndView;
     }
     /**
      * 内容跳转
      */
     @RequestMapping("/random-content-other/{articleId}/{timstamp}")
-    String randomContentOther(@PathVariable String articleId,@PathVariable String timstamp,Model model, HttpServletRequest request) {
+    public String randomContentOther(@PathVariable String articleId,@PathVariable String timstamp,Model model, HttpServletRequest request) {
         //获取域名集合map
         Map<String,Object> domainMap = (HashMap<String,Object>)iDomainWebService.queryDomainByredisServer(ConstFinal.DOMAIN_MAP);
         JSONObject jsonObject = WechatUtil.nextUrlBuild(WebConst.SUB_SHARE_DOMAIN, UrlConstant.PATH_JUMP_RUL,articleId,domainMap);
@@ -107,6 +114,33 @@ public class WechatController {
         log.info("----------分享链接为：{}",shareUrl);
         return "html/random-content-other";
     }
+
+    @RequestMapping("/jump/{articleId}/{timstamp}")
+    public String jumpOther(@PathVariable String articleId,@PathVariable String timstamp,Model model) {
+        //获取图片相关信息
+        ArticleWithImages articleWithImages = iArticleService.queryCurrentArticle(articleId);
+        //base64编码
+        model.addAttribute("article",articleWithImages);
+        return "html/random-common";
+    }
+    /**
+     * 无需强制分享普通界面
+     */
+    @RequestMapping("/random-common/{articleId}/{timstamp}")
+    public String randomCommon(@PathVariable String articleId, @PathVariable String timstamp, Model model, HttpServletRequest request) {
+        //获取域名集合map
+        Map<String,Object> domainMap = (HashMap<String,Object>)iDomainWebService.queryDomainByredisServer(ConstFinal.DOMAIN_MAP);
+        JSONObject jsonObject = WechatUtil.nextUrlBuild(WebConst.RONDOM_COMMON_DOMAIN, UrlConstant.PATH_WITHOUT_SHARE_RUL,articleId,domainMap);
+        String withoutShareUrl = String.valueOf(jsonObject.get("url"));
+//      //获取图片相关信息
+        ArticleWithImages articleWithImages = iArticleService.queryCurrentArticle(articleId);
+        //base64编码
+        model.addAttribute("article",articleWithImages);
+        model.addAttribute(UrlConstant.WITH_OUT_SHARE, Base64Util.encode(withoutShareUrl));
+        log.info("----------分享链接为：{}",withoutShareUrl);
+        return "html/random-common";
+    }
+
     private ArticleWithImages getArticleWithImages(@PathVariable String articleId, String domain) {
         //获取图片相关信息
         ArticleWithImages articleWithImages = iArticleService.queryCurrentArticle(articleId);
