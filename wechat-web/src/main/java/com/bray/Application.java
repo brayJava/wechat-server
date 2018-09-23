185b16e36bde5a088620741104155a76d8cde1e8
bray <goodboy_bray@163.com>
2018-09-23 17:00:58 +0800

    修改

--- wechat-web/src/main/java/com/bray/Application.java
+++ wechat-web/src/main/java/com/bray/Application.java
@@ -12,12 +12,12 @@ import javax.servlet.annotation.WebListener;
 @SpringBootApplication
 @WebListener
 @ServletComponentScan(basePackages = {"com.bray.web.filter"})
-public class WebApplication {
+public class Application {
 
     public static void main(String[] args) {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
-        SpringApplication.run(WebApplication.class,args);
+        SpringApplication.run(Application.class,args);
 
 
     }
