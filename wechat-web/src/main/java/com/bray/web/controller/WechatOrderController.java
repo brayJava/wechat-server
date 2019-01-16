e70bb901862fa7ccf360af3770f86ed6b6328684
bray <goodboy_bray@163.com>
2019-01-16 13:22:47 +0800

    修改

--- wechat-web/src/main/java/com/bray/web/controller/WechatOrderController.java
+++ wechat-web/src/main/java/com/bray/web/controller/WechatOrderController.java
@@ -96,10 +96,20 @@ public class WechatOrderController {
     /**
      * 订单跳转
      */
-    @RequestMapping("/jump-order")
-    String order() {
+    @RequestMapping(value="/jump-order",produces = "text/html;charset=utf-8")
+    @ResponseBody
+    String order(HttpServletRequest request, HttpServletResponse response, Model model) {
         iOrderWebService.insertOrderLog(OrderLogType.ORDER_TYPE);
-        return "order/order";
+        String showhtml = String.valueOf(redisObj.getRedisValueByKey("ORDERVAL99"));
+        if(!StringUtils.isEmpty(showhtml) && !"null".equals(showhtml)){
+            return  showhtml;
+        }
+        //手动渲染
+        SpringWebContext ctx = new SpringWebContext(request,response,
+                request.getServletContext(),request.getLocale(), model.asMap(), applicationContext );
+        showhtml = thymeleafViewResolver.getTemplateEngine().process("order/order", ctx);
+        redisObj.saveDataToRedis("ORDERVAL99",showhtml);
+        return showhtml;
     }
     /**
      * 订单跳转
@@ -109,58 +119,58 @@ public class WechatOrderController {
         iOrderWebService.insertOrderLog(OrderLogType.JINYOU_TYPE);
         return "order/jinyou/wenan";
     }
-    @RequestMapping("confirm-order")
-    @ResponseBody
-    public RestResponseBo orderConfirm(OrderModelVo orderModelVo) {
-        iOrderWebService.insert(orderModelVo);
-        try {
-            //发送email
-            sendEmailWithOrder(orderModelVo);
-        } catch (Exception e) {
-            log.error("---------email发送错误");
-            e.printStackTrace();
-        }
-        log.info("----订单数据...{}", JSONObject.toJSONString(orderModelVo));
-        return RestResponseBo.ok();
-    }
-
-
-    private void sendEmailWithOrder(OrderModelVo orderModelVo) {
-        StringBuilder orderBuf = new StringBuilder();
-//        orderBuf.append("------------新的订单------------- \n");
-        orderBuf.append("姓名："+orderModelVo.getName()+"\n");
-        orderBuf.append("电话："+orderModelVo.getPhone()+"\n");
-        orderBuf.append("城市："+orderModelVo.getProvince()+orderModelVo
-                .getCity()+orderModelVo.getCounty()+"\n");
-        orderBuf.append("地址："+orderModelVo.getAddress()+"\n");
-        orderBuf.append("商品类型："+orderModelVo.getTitle()+"\n");
-        orderBuf.append("尺寸大小："+orderModelVo.getSize()+"\n");
-        if(!StringUtils.isEmpty(orderModelVo.getMessage()))
-            orderBuf.append("顾客留言："+orderModelVo.getMessage());
-        MimeMessage msg = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = null;
-        try {
-            helper = new MimeMessageHelper(msg, true,"utf-8");
-            helper.setFrom("goodboy_bray@163.com");
-            helper.setCc("goodboy_bray@163.com");
-            helper.setTo(new String[]{"78901623@qq.com","937085200@qq.com","619105979@qq.com","527297994@qq.com","1194633142@qq.com","1318134732@qq.com"});
-            helper.setText(orderBuf.toString());
-            helper.setSubject("来了新的订单");
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-
-//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-//        simpleMailMessage.setSubject("有新的订单");
-//        simpleMailMessage.setText(orderBuf.toString());
-//        simpleMailMessage.setTo("1318134732@qq.com");
-//        simpleMailMessage.setFrom("goodboy_bray@163.com");
-//        Properties prop = new Properties();
-//        //解决ip与域名不匹配的问题
-//        prop.put("mail.smtp.localhost","mail.163.com");
-//        prop.put("mail.smtp.auth", "false");
-////        prop.put("mail.smtp.port", "25");
-//        javaMailSender.setJavaMailProperties(prop);
-        javaMailSender.send(msg);
-    }
+//     @RequestMapping("confirm-order")
+//     @ResponseBody
+//     public RestResponseBo orderConfirm(OrderModelVo orderModelVo) {
+//         iOrderWebService.insert(orderModelVo);
+//         try {
+//             //发送email
+//             sendEmailWithOrder(orderModelVo);
+//         } catch (Exception e) {
+//             log.error("---------email发送错误");
+//             e.printStackTrace();
+//         }
+//         log.info("----订单数据...{}", JSONObject.toJSONString(orderModelVo));
+//         return RestResponseBo.ok();
+//     }
+//
+//
+//     private void sendEmailWithOrder(OrderModelVo orderModelVo) {
+//         StringBuilder orderBuf = new StringBuilder();
+// //        orderBuf.append("------------新的订单------------- \n");
+//         orderBuf.append("姓名："+orderModelVo.getName()+"\n");
+//         orderBuf.append("电话："+orderModelVo.getPhone()+"\n");
+//         orderBuf.append("城市："+orderModelVo.getProvince()+orderModelVo
+//                 .getCity()+orderModelVo.getCounty()+"\n");
+//         orderBuf.append("地址："+orderModelVo.getAddress()+"\n");
+//         orderBuf.append("商品类型："+orderModelVo.getTitle()+"\n");
+//         orderBuf.append("尺寸大小："+orderModelVo.getSize()+"\n");
+//         if(!StringUtils.isEmpty(orderModelVo.getMessage()))
+//             orderBuf.append("顾客留言："+orderModelVo.getMessage());
+//         MimeMessage msg = javaMailSender.createMimeMessage();
+//         MimeMessageHelper helper = null;
+//         try {
+//             helper = new MimeMessageHelper(msg, true,"utf-8");
+//             helper.setFrom("goodboy_bray@163.com");
+//             helper.setCc("goodboy_bray@163.com");
+//             helper.setTo(new String[]{"78901623@qq.com","937085200@qq.com","619105979@qq.com","527297994@qq.com","1194633142@qq.com","1318134732@qq.com"});
+//             helper.setText(orderBuf.toString());
+//             helper.setSubject("来了新的订单");
+//         } catch (MessagingException e) {
+//             e.printStackTrace();
+//         }
+//
+// //        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+// //        simpleMailMessage.setSubject("有新的订单");
+// //        simpleMailMessage.setText(orderBuf.toString());
+// //        simpleMailMessage.setTo("1318134732@qq.com");
+// //        simpleMailMessage.setFrom("goodboy_bray@163.com");
+// //        Properties prop = new Properties();
+// //        //解决ip与域名不匹配的问题
+// //        prop.put("mail.smtp.localhost","mail.163.com");
+// //        prop.put("mail.smtp.auth", "false");
+// ////        prop.put("mail.smtp.port", "25");
+// //        javaMailSender.setJavaMailProperties(prop);
+//         javaMailSender.send(msg);
+//     }
 }
