8969926d45fcbd1638a31a838d528958211daa48
bray <goodboy_bray@163.com>
2019-01-22 19:27:30 +0800

    修改

--- wechat-admin/src/main/java/com/bray/web/controller/IndexController.java
+++ wechat-admin/src/main/java/com/bray/web/controller/IndexController.java
@@ -7,12 +7,19 @@ import com.bray.model.WyOrderLog;
 import com.bray.service.IOrderAdminService;
 import lombok.extern.slf4j.Slf4j;
 import org.omg.CORBA.Object;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.spring4.context.SpringWebContext;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -34,6 +41,11 @@ public class IndexController {
 
     @Resource
     private RedisPoolCache redisObj;
+    @Autowired
+    private ThymeleafViewResolver thymeleafViewResolver;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @RequestMapping("/wzylm")
     public String home() {
@@ -59,4 +71,33 @@ public class IndexController {
         model.addAttribute("orderlist",orderlist);
         return "index/welcome";
     }
+    /**
+     * 订单日志查询
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     */
+    @RequestMapping(value="/ssckOrder",produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String ssckOrder(HttpServletRequest request, HttpServletResponse response, Model model) {
+        List<String> list = redisObj.lrangeRedis("orderlist", 0, 20);
+        List<OrderModelVo> orderModelVos = new ArrayList<>();
+        if(!CollectionUtils.isEmpty(list)) {
+            list.stream().forEach(x ->{
+                OrderModelVo y = JSONObject.parseObject(x, OrderModelVo.class);
+                if(Objects.isNull(y .getOrderDate())) {
+                    y.setOrderDate(new Date());
+                }
+                orderModelVos.add(y);
+            });
+        }
+        model.addAttribute("orderlist",orderModelVos);
+        //手动渲染
+        SpringWebContext ctx = new SpringWebContext(request,response,
+                request.getServletContext(),request.getLocale(), model.asMap(), applicationContext );
+        String html = thymeleafViewResolver.getTemplateEngine().process("order/order-log", ctx);
+        return html;
+    }
+
 }
